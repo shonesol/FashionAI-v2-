a34cfab68aaeb63c023d5a10e48087586f4aca53
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-14 00:57:05 +0300

    Update local-database.js

--- local-database.js
+++ local-database.js
@@ -1,88 +1,6 @@
 // ===============================
-// Toggle Favorite Outfit
-// ===============================
-
-export function toggleFavoriteOutfit(id){
-
-
-return new Promise((resolve,reject)=>{
-
-
-const transaction =
-db.transaction(
-OUTFIT_STORE,
-"readwrite"
-);
-
-
-
-const store =
-transaction.objectStore(
-OUTFIT_STORE
-);
-
-
-
-const request =
-store.get(id);
-
-
-
-
-request.onsuccess=()=>{
-
-
-const outfit =
-request.result;
-
-
-
-if(outfit){
-
-
-outfit.favorite =
-!outfit.favorite;
-
-
-
-store.put(outfit);
-
-
-
-resolve(outfit);
-
-
-}
-
-else{
-
-
-reject(
-"Outfit not found"
-);
-
-
-}
-
-
-};
-
-
-});
-
-
-}
-
-
-
-
-
-
-
-// ===============================
 // Get Favorite Outfits
 // ===============================
-
 
 export function getFavoriteOutfits(){
 
@@ -142,7 +60,21 @@ reject(false);
 });
 
 
-}){
+}
+
+
+
+
+
+
+// ===============================
+// Update Clothing
+// ===============================
+
+export function updateClothing(
+id,
+changes
+){
 
 
 return new Promise((resolve,reject)=>{
@@ -212,10 +144,7 @@ reject(
 }
 
 
-
 };
-
-
 
 
 
@@ -225,297 +154,6 @@ request.onerror=()=>{
 reject(
 "Update failed"
 );
-
-
-};
-
-
-
-});
-
-
-}
-
-
-
-
-
-
-
-
-
-// ===============================
-// Delete Clothing
-// ===============================
-
-
-export function deleteClothing(id){
-
-
-return new Promise((resolve)=>{
-
-
-const transaction =
-db.transaction(
-WARDROBE_STORE,
-"readwrite"
-);
-
-
-
-const store =
-transaction.objectStore(
-WARDROBE_STORE
-);
-
-
-
-store.delete(id);
-
-
-
-resolve(true);
-
-
-
-});
-
-
-}
-
-
-
-
-
-
-
-
-
-// ===============================
-// Save Outfit History
-// ===============================
-
-
-export function saveOutfit(outfit){
-
-
-return new Promise((resolve,reject)=>{
-
-
-const transaction =
-db.transaction(
-OUTFIT_STORE,
-"readwrite"
-);
-
-
-
-const store =
-transaction.objectStore(
-OUTFIT_STORE
-);
-
-
-
-
-const data = {
-
-
-...outfit,
-
-
-date:
-new Date()
-.toISOString(),
-
-
-favorite:false
-
-
-};
-
-
-
-
-
-const request =
-store.add(data);
-
-
-
-
-request.onsuccess=()=>{
-
-
-resolve(true);
-
-
-};
-
-
-
-
-request.onerror=()=>{
-
-
-reject(false);
-
-
-};
-
-
-
-});
-
-
-}
-
-
-
-
-
-
-
-
-
-// ===============================
-// Get Outfit History
-// ===============================
-
-
-export function getOutfitHistory(){
-
-
-return new Promise((resolve,reject)=>{
-
-
-const transaction =
-db.transaction(
-OUTFIT_STORE,
-"readonly"
-);
-
-
-
-const store =
-transaction.objectStore(
-OUTFIT_STORE
-);
-
-
-
-const request =
-store.getAll();
-
-
-
-
-request.onsuccess=()=>{
-
-
-resolve(
-request.result
-);
-
-
-};
-
-
-
-
-request.onerror=()=>{
-
-
-reject(false);
-
-
-};
-
-
-
-});
-
-
-}
-
-
-
-
-
-
-
-
-
-// ===============================
-// Rate Outfit
-// ===============================
-
-
-export function rateOutfit(
-id,
-rating
-){
-
-
-return new Promise((resolve,reject)=>{
-
-
-const transaction =
-db.transaction(
-OUTFIT_STORE,
-"readwrite"
-);
-
-
-
-const store =
-transaction.objectStore(
-OUTFIT_STORE
-);
-
-
-
-const request =
-store.get(id);
-
-
-
-
-request.onsuccess=()=>{
-
-
-const outfit =
-request.result;
-
-
-
-if(outfit){
-
-
-outfit.rating =
-rating;
-
-
-
-store.put(outfit);
-
-
-
-resolve(outfit);
-
-
-}
-
-else{
-
-
-reject(
-"Outfit not found"
-);
-
-
-}
-
 
 
 };
