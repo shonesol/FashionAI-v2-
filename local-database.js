83e5e104c0c284ec89f95a74ee8ef9c5d3701eee
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-14 00:03:37 +0300

    Create local-database.js

---
+++ local-database.js
@@ -0,0 +1,250 @@
+// FashionAI Local Database
+// Stores wardrobe on user's device
+
+
+const DATABASE_NAME = "FashionAI_DB";
+
+const DATABASE_VERSION = 1;
+
+const STORE_NAME = "wardrobe";
+
+
+
+let db;
+
+
+
+// Open database
+
+export function openDatabase(){
+
+
+return new Promise((resolve,reject)=>{
+
+
+const request =
+indexedDB.open(
+DATABASE_NAME,
+DATABASE_VERSION
+);
+
+
+
+request.onupgradeneeded = (event)=>{
+
+
+db = event.target.result;
+
+
+
+if(!db.objectStoreNames.contains(STORE_NAME)){
+
+
+const store =
+db.createObjectStore(
+STORE_NAME,
+{
+keyPath:"id",
+autoIncrement:true
+}
+);
+
+
+store.createIndex(
+"category",
+"category",
+{
+unique:false
+}
+);
+
+
+store.createIndex(
+"color",
+"color",
+{
+unique:false
+}
+);
+
+
+}
+
+
+};
+
+
+
+request.onsuccess=(event)=>{
+
+
+db =
+event.target.result;
+
+
+resolve(db);
+
+
+};
+
+
+
+request.onerror=(error)=>{
+
+
+reject(error);
+
+
+};
+
+
+});
+
+
+}
+
+
+
+
+
+
+// Save clothing item
+
+export function saveClothing(item){
+
+
+return new Promise((resolve,reject)=>{
+
+
+const transaction =
+db.transaction(
+STORE_NAME,
+"readwrite"
+);
+
+
+const store =
+transaction.objectStore(
+STORE_NAME
+);
+
+
+
+const request =
+store.add(item);
+
+
+
+request.onsuccess=()=>{
+
+
+resolve(true);
+
+
+};
+
+
+
+request.onerror=()=>{
+
+
+reject(false);
+
+
+};
+
+
+});
+
+
+}
+
+
+
+
+
+
+// Get all clothes
+
+export function getAllClothes(){
+
+
+return new Promise((resolve)=>{
+
+
+const transaction =
+db.transaction(
+STORE_NAME,
+"readonly"
+);
+
+
+
+const store =
+transaction.objectStore(
+STORE_NAME
+);
+
+
+
+const request =
+store.getAll();
+
+
+
+request.onsuccess=()=>{
+
+
+resolve(
+request.result
+);
+
+
+};
+
+
+
+});
+
+
+}
+
+
+
+
+
+
+// Delete clothing
+
+export function deleteClothing(id){
+
+
+return new Promise((resolve)=>{
+
+
+const transaction =
+db.transaction(
+STORE_NAME,
+"readwrite"
+);
+
+
+
+const store =
+transaction.objectStore(
+STORE_NAME
+);
+
+
+
+store.delete(id);
+
+
+
+resolve(true);
+
+
+});
+
+
+}
