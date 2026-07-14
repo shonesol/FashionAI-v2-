a126d36c0759c2e44cdd86ca6981e9d295c3aa10
TAMALE SHONE <70106850+shonesol@users.noreply.github.com>
2026-07-14 11:07:33 +0300

    Update and rename local-database.js to fashion-ai.js

---
+++ fashion-ai.js
@@ -0,0 +1,35 @@
+// =================================
+// FashionAI Local Analyzer
+// Offline Fashion Intelligence
+// =================================
+
+
+export function localAnalyze(image){
+
+
+return {
+
+
+localDetected:true,
+
+
+category:
+"clothing",
+
+
+basicStyle:
+"casual",
+
+
+uploadedAt:
+new Date().toISOString(),
+
+
+confidence:
+0.5
+
+
+};
+
+
+}
